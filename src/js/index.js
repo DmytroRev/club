991b558241d2e7c221c944067a4eb1901e2338b1
Dmytro Reviakin <thefaust31@gmail.com>
2025-02-07 01:19:46 +0200

    add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,61 @@
 import "../styles/main.scss";
-import emailjs from 'emailjs-com';
-
+import emailjs from "emailjs-com";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/effect-fade";
 
 const button = document.querySelector(".header__btn-menu");
 const body = document.querySelector("body");
 const navHeader = document.querySelector(".header__list");
-const menuBg = document.querySelector('.wrapper-menu')
-const link = document.querySelector('.list-link')
+const menuBg = document.querySelector(".wrapper-menu");
+const link = document.querySelector(".list-link");
 
-button.addEventListener("click", () => {
-  body.classList.toggle("menu-active");
-  menuBg.classList.toggle('bg-decor')
-  link.classList.toggle('text-warning')
-});
+if (button) {
+  button.addEventListener("click", () => {
+    body.classList.toggle("menu-active");
+    menuBg?.classList.toggle("bg-decor");
+    link?.classList.toggle("text-warning");
+  });
+}
 
-navHeader.addEventListener("click", () => {
-  body.classList.remove("menu-active");
-});
+if (navHeader) {
+  navHeader.addEventListener("click", () => {
+    body.classList.remove("menu-active");
+  });
+}
 
-document.getElementById('contact-form').addEventListener('submit', (e) => {
-  e.preventDefault()
+// document.getElementById("contact-form").addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  const status = document.getElementById('status')
+//   const status = document.getElementById("status");
 
-  emailjs.sendForm(
-    'service_0j588gc',
-    'template_bq6jkme',
-    '#contact-form',
-    'O15zWuG0SMs10Kuy1'
-  ).then(
-    () => {
-      status.textContent = 'Message send!'
-      e.target.reset()
-    },
-    (error) => {
-      status.textContent = `Error send ${error.text}`
-    }
-  )
-})
+//   emailjs
+//     .sendForm(
+//       "service_0j588gc",
+//       "template_bq6jkme",
+//       "#contact-form",
+//       "O15zWuG0SMs10Kuy1"
+//     )
+//     .then(
+//       () => {
+//         status.textContent = "Message send!";
+//         e.target.reset();
+//       },
+//       (error) => {
+//         status.textContent = `Error send ${error.text}`;
+//       }
+//     );
+// });
+
+// const swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   effect: "fade",
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
